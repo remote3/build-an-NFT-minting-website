6d5f508fe0ee3c43baf2ef82ed85b278a2fd7184
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2022-02-15 10:20:16 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# build-an-NFT-minting-website
-Learn to build a NFT minting website using Solidity & React JS through our guided tutorial.
+# Simple NFT minting dApp
 
-Visit: https://remote3.co/learn-web3
-
-![nft-minting-site](https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1643900051171x692665464821051700%2FNFT%2520SITE.png?w=384&h=217&auto=compress&dpr=2&fit=max)
+![screen-capture (2).gif](https://cdn.hashnode.com/res/hashnode/image/upload/v1644777454518/4As5ak665.gif)
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { getAccount } from "simple-web3";
+import './App.css'
+import { CONTRACT_ABI, CONTRACT_ADDRESS, GAS_LIMIT } from "./config";
+
+function App() {
+  const [loading, setLoading] = useState(false)
+
+  const mintNFT = async () => {
+
+    try {
+      setLoading(true)
+      let ACCOUNT = await getAccount()
+      const contract = new window.web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS)
+
+      await contract.methods.mint().send({
+        from: ACCOUNT,
+        gasLimit: GAS_LIMIT,
+        value: 0
+      })
+
+      setLoading(false)
+      alert('Yayy! minted a GoldenMan')
+    } catch (e) {
+      setLoading(false)
+      console.log(e)
+    }
+  }
+
+  return (
+    loading ?
+
+      <div className="App">
+        <h1>Minting</h1>
+      </div >
+
+      : <div className="App">
+        <h1>Mint my NFT</h1>
+        <button onClick={mintNFT}>Mint an NFt</button>
+      </div >
+  );
+}
+
+export default App;
